3a88ce34fc8889d948e3c06fc2baa69baa0bee7a
crickabhi <abhinav@eckovation.com>
2016-11-20 03:47:10 +0530

    Master story card created

--- RopossoChallenge/MasterViewController.swift
+++ RopossoChallenge/MasterViewController.swift
@@ -11,16 +11,36 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
-
-
+    var userObjects = [AnyObject]()
+    var storyObjects = [AnyObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        /*
         self.navigationItem.leftBarButtonItem = self.editButtonItem
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
         self.navigationItem.rightBarButtonItem = addButton
+        */
+        
+        if let path = Bundle.main.path(forResource: "iOS_Data", ofType: "json")
+        {
+            if let jsonData = NSData(contentsOfFile: path)//(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)
+            {
+                do{
+                    let jsonResult = try JSONSerialization.jsonObject(with: jsonData as Data, options: JSONSerialization.ReadingOptions.mutableContainers)
+                    userObjects = jsonResult as! [AnyObject]
+                    storyObjects = jsonResult as! [AnyObject]
+                }
+                catch
+                {
+                    print("error")
+                }
+                
+            }
+        }
+        
         if let split = self.splitViewController {
             let controllers = split.viewControllers
             self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -36,19 +56,19 @@ class MasterViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+/*
     func insertNewObject(_ sender: Any) {
         objects.insert(NSDate(), at: 0)
         let indexPath = IndexPath(row: 0, section: 0)
         self.tableView.insertRows(at: [indexPath], with: .automatic)
     }
-
+*/
     // MARK: - Segues
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object = storyObjects[indexPath.row] as! NSDate
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
@@ -64,15 +84,26 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return storyObjects.count - 2
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        /*
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
         let object = objects[indexPath.row] as! NSDate
         cell.textLabel!.text = object.description
         return cell
+        */
+        let cell = StoryCard(style: UITableViewCellStyle.default, reuseIdentifier: "myIdentifier")
+        cell.storyTitle.text = storyObjects[indexPath.row + 2].value(forKey: "title") as? String
+        cell.storyImage.sd_setImage(with: NSURL(string:(storyObjects[indexPath.row + 2].value(forKey: "si") as? String)!)! as URL)
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250
     }
 
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -82,7 +113,7 @@ class MasterViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
+            storyObjects.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
---
+++ RopossoChallenge/StoryCard.swift
@@ -0,0 +1,54 @@
+//
+//  StoryCard.swift
+//  RopossoChallenge
+//
+//  Created by Abhinav Mathur on 20/11/16.
+//  Copyright © 2016 Abhinav Mathur. All rights reserved.
+//
+
+import UIKit
+
+class StoryCard: UITableViewCell {
+
+    var storyTitle: UILabel!
+    var storyImage : UIImageView!
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:)")
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let gap : CGFloat = 10
+        let labelHeight: CGFloat = 30
+        let labelWidth: CGFloat = UIScreen.main.bounds.width - 10
+        let lineGap : CGFloat = 5
+        let label2Y : CGFloat = gap + labelHeight + lineGap
+        let imageSize : CGFloat = 200
+        
+        storyTitle = UILabel()
+        storyTitle.frame = CGRect(x: gap, y: gap, width: labelWidth, height: labelHeight)
+        storyTitle.textColor = UIColor.black
+        storyTitle.font = UIFont(name: "HelveticaNeue", size: 14)
+        contentView.addSubview(storyTitle)
+        
+        storyImage = UIImageView()
+        storyImage.frame = CGRect(x: gap,y: label2Y, width: labelWidth,height: imageSize)
+        storyImage.contentMode = .scaleAspectFit
+        contentView.addSubview(storyImage)
+        
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
